564ef662407774ae97dfbf9be445ae72f5e48f45
imam syafii <imam@importir.com>
2024-02-13 07:29:22 +0700

    create a middleware and a function to handle itt

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express();
+
 app.use(express.json())
 
 app.listen(3000, () => {
@@ -21,3 +22,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
